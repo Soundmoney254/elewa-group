d3539677fd26dd5f776c209cebadf29a0e720e9a
Samuel <samnmbuguah@gmail.com>
2023-11-03 10:21:29 +0300

    ✨Create Hero section files in MVVM architecture

---
+++ src/Views/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/HeroSection.css';
+import Header from './Header';
+
+function HeroSection() {
+    return (
+        <div className='hero-section'>
+            <Header />
+        </div>
+    )
+}
+
+export default HeroSection
