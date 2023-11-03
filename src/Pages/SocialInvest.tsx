bad01e080996bb289cd5b046d0f80be7c8cb2b98
Samuel <samnmbuguah@gmail.com>
2023-11-03 18:54:00 +0300

    ✨ Add SocialInvest page with HeroSection and RecordSection components.

---
+++ src/Pages/SocialInvest.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../Views/HeroSection";
+import RecordSection from "../Views/RecordSection";
+
+function SocialInvest() {
+    return (
+        <div className="homepage">
+            <HeroSection />
+            <RecordSection />
+        </div>
+    )
+}
+
+export default SocialInvest
