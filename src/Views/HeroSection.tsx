78a41aac8be6677de145ee8c329e888de8f50668
Samuel <samnmbuguah@gmail.com>
2023-11-03 11:46:17 +0300

    ✨ Add hero data to HeroSection component

--- src/Views/HeroSection.tsx
+++ src/Views/HeroSection.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import '../Styles/HeroSection.css';
 import Header from './Header';
+import { useHeroData } from '../ViewModels/useHeroData';
 
 function HeroSection() {
+    const heroData = useHeroData();
+    console.log (heroData);
     return (
         <div className='hero-section'>
             <Header />
+            <p>{heroData?.scalingImpact}</p>
+            <p>{heroData?.humanAndEnvironmentalImpact}</p>
         </div>
     )
 }
