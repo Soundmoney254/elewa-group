5e5b77110b41b1e0473e4dffbc73494d836315d7
Samuel <samnmbuguah@gmail.com>
2023-11-03 18:58:04 +0300

    💄 Add styling and restructure record section layout

--- src/Views/RecordSection.tsx
+++ src/Views/RecordSection.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import useRecordData from '../ViewModels/useRecordData';
+import '../Styles/RecordSection.css';
 
 const RecordSection: React.FC = () => {
     const recordData = useRecordData();
 
     return (
-        <div>
-            <h2>Record Section</h2>
-            <p>{recordData.peopleTrained}</p>
-            <p>{recordData.careersLaunched}</p>
-            <p>{recordData.projectsFinished}</p>
-            <p>{recordData.investors}</p>
+        <div className="record-section">
+            <div className='record'>
+                <p>{recordData.peopleTrained}</p>
+                <p>{recordData.careersLaunched}</p>
+                <p>{recordData.projectsFinished}</p>
+                <p>{recordData.investors}</p>
+            </div>
+            <div className='record-text'>
+                <p>{recordData.peopleTrainedText}</p>
+                <p>{recordData.careersLaunchedText}</p>
+                <p>{recordData.projectsFinishedText}</p>
+                <p>{recordData.investorsText}</p>
+            </div>
+            <img className='record-photo' src={recordData.recordPhoto} alt='record' />
         </div>
     );
 }
