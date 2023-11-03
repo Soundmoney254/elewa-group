6f59ebeac55046317f13ae3c09a77a7acdc6a8b2
Samuel <samnmbuguah@gmail.com>
2023-11-03 18:55:36 +0300

    ✨ Add new fields and update RecordData interface.

--- src/Models/RecordData.ts
+++ src/Models/RecordData.ts
@@ -3,4 +3,11 @@ export interface RecordData {
     careersLaunched: string;
     projectsFinished: string;
     investors: string;
+    peopleTrainedText: string;
+    careersLaunchedText: string;
+    projectsFinishedText: string;
+    investorsText: string;
+    recordPhoto: string;
+    recordSectionTitle: string;
+    recordSectionParagraph: string;
 }
