1ca76f402d5cbc396b9a6d4d582377499fe53f71
Samuel <samnmbuguah@gmail.com>
2023-11-03 14:06:19 +0300

    ✨ Create the social invest page

---
+++ src/Pages/SocialInvest.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../Views/HeroSection";
+import RecordSection from "../Views/RecordSection";
+
+function SocialInvest() {
+    return (
+        <div>
+            <HeroSection />
+            <RecordSection />
+        </div>
+    )
+}
+
+export default SocialInvest
