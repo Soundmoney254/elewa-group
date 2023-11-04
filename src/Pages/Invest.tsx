faa0a5084935738a214761e13941cd52fc02e0dd
Samuel <samnmbuguah@gmail.com>
2023-11-04 17:45:54 +0300

    ✨ Add Invest page with HeroSection and Footer components.

---
+++ src/Pages/Invest.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../Views/HeroSection';
+import Footer from '../Views/Footer';
+
+function Invest() {
+    return (
+        <div>
+            <HeroSection index={2} />
+            <Footer />
+        </div>
+    )
+}
+
+export default Invest
