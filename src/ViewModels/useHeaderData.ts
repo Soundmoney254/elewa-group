32f01769f3f64b3398abb0ff95dbfc0d7b891b8c
Samuel <samnmbuguah@gmail.com>
2023-11-04 06:11:09 +0300

    ⚡️ Fix import and add utility function for URL validation in useHeaderData.ts

--- src/ViewModels/useHeaderData.ts
+++ src/ViewModels/useHeaderData.ts
@@ -1,18 +1,10 @@
 import { useEffect, useState } from 'react';
-import { LogoUrl } from '../Models/HeaderData';
+import { LogoUrl } from '../Models/Header';
 import data from '../data/data.json';
+import { isValidUrl } from '../Utilities/isValidUrl';
 
 export const useHeaderData = () => {
     const [logoUrl, setLogoUrl] = useState<LogoUrl>();
-
-    const isValidUrl = (url: string): boolean => {
-        try {
-            new URL(url);
-            return true;
-        } catch (_) {
-            return false;
-        }
-    };
 
     useEffect(() => {
         if (isValidUrl(data.logoUrl)) {
