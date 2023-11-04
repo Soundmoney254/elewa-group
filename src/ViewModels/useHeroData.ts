ed1e17dbb4ff8e16f6bc411300c2e373844c3e5f
Samuel <samnmbuguah@gmail.com>
2023-11-04 06:08:09 +0300

    ⚡️ Update HeroData import and add isValidUrl utility function model.

--- src/ViewModels/useHeroData.ts
+++ src/ViewModels/useHeroData.ts
@@ -1,18 +1,10 @@
 import data from '../data/data.json';
 import { useEffect, useState } from 'react';
-import { HeroData } from '../Models/HeroData';
+import { HeroData } from '../Models/Hero';
+import { isValidUrl } from '../Utilities/isValidUrl';
 
 export const useHeroData = () => {
     const [heroData, setHeroData] = useState<HeroData>();
-
-    const isValidUrl = (url: string): boolean => {
-        try {
-            new URL(url);
-            return true;
-        } catch (_) {
-            return false;
-        }
-    };
 
     useEffect(() => {
         if (isValidUrl(data.heroData.heroImage)) {
