31ea1841e27187c573500a7af682e2199bc75474
Samuel <samnmbuguah@gmail.com>
2023-11-04 16:52:21 +0300

    🎨 Update heroData hook in HeroSection component

--- src/Views/HeroSection.tsx
+++ src/Views/HeroSection.tsx
@@ -6,11 +6,11 @@ import useIntersectionObserver from '../Utilities/useIntersectionObserver';
 import "../Styles/Animate.css"
 
 function HeroSection() {
-    const heroData = useHeroData();
+    const heroData = useHeroData(0);
     const ref = useIntersectionObserver();
 
     return (
-        <div ref={ref} className='hero-section'>
+        <div ref={ref} className='hero-section' style={{ backgroundImage: `url(${heroData?.heroImage})` }} >
             <Header />
             <section className='hero'>
                 <h3 className='scaling'>{heroData?.scalingImpact}</h3>
