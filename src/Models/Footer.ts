0025f7f9d6de925bccbd582942f4bbc94eb32d31
Samuel <samnmbuguah@gmail.com>
2023-11-04 11:57:56 +0300

    ✨ Add FooterData interface to Footer.ts

---
+++ src/Models/Footer.ts
@@ -0,0 +1,12 @@
+export interface FooterData {
+    Headquarters: {
+        address: string;
+    };
+    Contact: {
+        phone: string;
+        email: string;
+    };
+    Navigation: string[];
+    Brands: string[];
+    Privacy: string[];
+}
