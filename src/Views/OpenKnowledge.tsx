227a92967432af8428c4120f82350ed931362011
Samuel <samnmbuguah@gmail.com>
2023-11-04 10:55:19 +0300

    ✨ Add OpenKnowledge component to Views directory

---
+++ src/Views/OpenKnowledge.tsx
@@ -0,0 +1,13 @@
+import LinkTitleParagraphPage from "../Utilities/LinkTitleParagraphPage";
+import React from 'react';
+import '../Styles/OpenKnowledge.css';
+
+function OpenKnowledge() {
+  return (
+    <div className="open-knowledge-section">
+        <LinkTitleParagraphPage index={3} />
+    </div>
+  )
+}
+
+export default OpenKnowledge;
