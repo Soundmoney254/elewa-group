0a61b0d2303032a88f1286c4f1803c44d576a39b
Samuel <samnmbuguah@gmail.com>
2023-11-04 17:45:42 +0300

    ✨ Add About page with HeroSection and Footer components.

---
+++ src/Pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../Views/HeroSection';
+import Footer from '../Views/Footer';
+
+function About() {
+    return (
+        <div>
+            <HeroSection index={1} />
+            <Footer />
+        </div>
+    )
+}
+
+export default About
