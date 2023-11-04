12aaa479b739b0595b6446a899de01038bad68a3
Samuel <samnmbuguah@gmail.com>
2023-11-04 10:57:41 +0300

    ✨ Add BeyondBusiness view component.

---
+++ src/Views/BeyondBusiness.tsx
@@ -0,0 +1,13 @@
+import LinkTitleParagraphPage from "../Utilities/LinkTitleParagraphPage";
+import React from 'react';
+import '../Styles/BeyondBusiness.css';
+
+function BeyondBusiness() {
+  return (
+    <div className="beyond-business-section">
+        <LinkTitleParagraphPage index={2} />
+    </div>
+  )
+}
+
+export default BeyondBusiness;
