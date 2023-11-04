f40cae3b816d3356fbbea9fe4e2a529edf66bb4f
Samuel <samnmbuguah@gmail.com>
2023-11-04 06:05:56 +0300

    ✨ Add MindsetSection to SocialInvest page

--- src/Pages/SocialInvest.tsx
+++ src/Pages/SocialInvest.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../Views/HeroSection";
 import RecordSection from "../Views/RecordSection";
+import MindsetSection from "../Views/Mindset";
 
 function SocialInvest() {
     return (
         <div className="homepage">
             <HeroSection />
             <RecordSection />
+            <MindsetSection />
         </div>
     )
 }
