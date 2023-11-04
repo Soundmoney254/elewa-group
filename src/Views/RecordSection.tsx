d676bb9877d3d1ccd2654d0aebaa43748dd2f683
Samuel <samnmbuguah@gmail.com>
2023-11-04 06:07:16 +0300

    ♻️ Add LinkTitleParagraphPage component to RecordSection

--- src/Views/RecordSection.tsx
+++ src/Views/RecordSection.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
 import useRecordData from '../ViewModels/useRecordData';
 import '../Styles/RecordSection.css';
+import LinkTitleParagraphPage from "../Utilities/LinkTitleParagraphPage";
 
 const RecordSection: React.FC = () => {
     const recordData = useRecordData();
 
-    return (
-        <div className="record-section">
-            <div className='record'>
-                <p>{recordData.peopleTrained}</p>
-                <p>{recordData.careersLaunched}</p>
-                <p>{recordData.projectsFinished}</p>
-                <p>{recordData.investors}</p>
-            </div>
-            <div className='record-text'>
-                <p>{recordData.peopleTrainedText}</p>
-                <p>{recordData.careersLaunchedText}</p>
-                <p>{recordData.projectsFinishedText}</p>
-                <p>{recordData.investorsText}</p>
-            </div>
-            <div className='record-section-two'>
-            <img className='record-photo' src={recordData.recordPhoto} alt='record' />
-            <div className='social-business'>
-                <h1>{recordData.recordSectionTitle}</h1>
-                <p>{recordData.recordSectionParagraph}</p>
-            </div>
-            </div>
-         
-       
 
+    return (
+        <div className="record-section-container">
+            <div className='record-section-one'>
+                <div className='record'>
+                    <p>{recordData.peopleTrained}</p>
+                    <p>{recordData.careersLaunched}</p>
+                    <p>{recordData.projectsFinished}</p>
+                    <p>{recordData.investors}</p>
+                </div>
+                <div className='record-text'>
+                    <p>{recordData.peopleTrainedText}</p>
+                    <p>{recordData.careersLaunchedText}</p>
+                    <p>{recordData.projectsFinishedText}</p>
+                    <p>{recordData.investorsText}</p>
+                </div>
+            </div>
+            <LinkTitleParagraphPage index={0} />
         </div>
     );
 }
