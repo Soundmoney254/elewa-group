828bd0ba21e74d00f93e1df50a7897bceeba6a69
Samuel <samnmbuguah@gmail.com>
2023-11-04 15:26:29 +0300

    🦺 Add validation for social media logo URLs in useFooterData hook

--- src/ViewModels/useFooterData.ts
+++ src/ViewModels/useFooterData.ts
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
 import { FooterData } from '../Models/Footer';
 import data from '../data/footer.json';
+import { isValidUrl } from '../Utilities/isValidUrl';
 
 export const useFooterData = () => {
     const [footerViewModel, setFooterViewModel] = useState<FooterData>();
 
     useEffect(() => {
         const footerData: FooterData = data;
-        setFooterViewModel(footerData);
+
+        // Check if the logos are valid URLs
+        const isValidFacebookUrl = isValidUrl(footerData.SocialMediaLogos.facebook);
+        const isValidInstagramUrl = isValidUrl(footerData.SocialMediaLogos.instagram);
+        const isValidLinkedinUrl = isValidUrl(footerData.SocialMediaLogos.linkedin);
+
+        // If all URLs are valid, assign the object to state
+        if (isValidFacebookUrl && isValidInstagramUrl && isValidLinkedinUrl) {
+            setFooterViewModel(footerData);
+        }
     }, []);
 
     return footerViewModel;
