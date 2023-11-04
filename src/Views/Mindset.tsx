04c1835ea9464b9225efbd87e17c67b70e0f5f3f
Samuel <samnmbuguah@gmail.com>
2023-11-04 06:06:25 +0300

    ✨ Add MindsetSection component to Views directory

---
+++ src/Views/Mindset.tsx
@@ -0,0 +1,13 @@
+import LinkTitleParagraphPage from "../Utilities/LinkTitleParagraphPage";
+import React from 'react';
+import '../Styles/MindsetSection.css';
+
+function MindsetSection() {
+  return (
+    <div className="mindset-section">
+        <LinkTitleParagraphPage index={1} />
+    </div>
+  )
+}
+
+export default MindsetSection
