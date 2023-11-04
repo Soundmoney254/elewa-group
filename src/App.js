c14c3cc320230204b550753fa914c94c5c5f7829
Samuel <samnmbuguah@gmail.com>
2023-11-04 17:44:44 +0300

    ✨ Add react-router-dom and update App.js to use it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SocialInvest from "./Pages/SocialInvest";
+import About from "./Pages/About";
+import Invest from "./Pages/Invest";
+
 function App() {
   return (
-    <div className="App">
-      <SocialInvest />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/invest" element={<Invest />} />
+          <Route path="/" element={<SocialInvest />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
