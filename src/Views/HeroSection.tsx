885c657dc8bb72b848d84b2eb14b91ed13cfe722
Samuel <samnmbuguah@gmail.com>
2023-11-04 17:47:00 +0300

    ♻️ Refactor HeroSection component to accept index prop.

--- src/Views/HeroSection.tsx
+++ src/Views/HeroSection.tsx
@@ -4,17 +4,22 @@ import Header from './Header';
 import { useHeroData } from '../ViewModels/useHeroData';
 import useIntersectionObserver from '../Utilities/useIntersectionObserver';
 import "../Styles/Animate.css"
+interface HeroSectionProps {
+    index: number;
+}
 
-function HeroSection() {
-    const heroData = useHeroData(0);
+function HeroSection({ index }: HeroSectionProps) {
+    const heroData = useHeroData(index);
     const ref = useIntersectionObserver();
 
     return (
-        <div ref={ref} className='hero-section' style={{ backgroundImage: `url(${heroData?.heroImage})` }} >
+        <div ref={ref} className='hero-section' style={{
+            backgroundImage: `url(${heroData?.heroImage})`,
+        }}>
             <Header />
             <section className='hero'>
-                <h3 className='scaling'>{heroData?.scalingImpact}</h3>
-                <h1 className='impact'>{heroData?.humanAndEnvironmentalImpact}</h1>
+                <h3 className='scaling'>{heroData?.heroSubheading}</h3>
+                <h1 className='impact'>{heroData?.heroHeading}</h1>
             </section>
         </div>
     )
